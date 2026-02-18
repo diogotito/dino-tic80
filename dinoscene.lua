23d7f2474d05f1b4d9bd5d9696ca9b68d84488f0
diogotito <diogotitomarques@gmail.com>
2026-02-18 00:54:20 +0000

    Extract unused "Vector2" "class" to a separate file

--- dinoscene.lua
+++ dinoscene.lua
@@ -24,72 +24,14 @@ local RAM = {
 	SCREEN_OFFSET  = 0x3FF9,
 }
 
--------------------------------------
---- a "class" for "Vector2"s
-local V2 = {}
-V2.__index = V2 -- setup inherintance
-
---- Shorthand for building {x=x, y=y}
---- with V2 as the metatable,
---- i.e., a proper "v2" from here on.
---- x and y can be omitted a la GLSL:
----   v2(1)        -> v2(1, 1)
----   v2()         -> v2(0, 0)
---- you can pass another table or V2:
----   v2{x=1, y=2} -> v2(1, 2)
-local function v2(x, y)
- if type(x)=="table" then -- v2{...}
- 	x, y = x.x or 0, x.y or 0
- end
-	if not x then     -- e.g. v2()
-		x = 0
-		y = 0
-	elseif not y then -- e.g. v2(1)
-	 y = x
-	end
-	return setmetatable({x=x, y=y}, V2)
-end
-
---- Overload the (+) operator for V2s
-function V2:__add(v)
-	return v2(self.x+v.x, self.y+v.y)
-end
-
---- overload the (*) operator for V2s
-function V2.__mul(u, v)
-	if type(u)=="number"then u=v2(u) end
-	if type(v)=="number"then v=v2(v) end
-	return v2(u.x*v.x, u.y*v.y)
-end
-
---- *(SHORTHAND)*
---- Get x,y as a list of expressions
-function V2:unpack()
-	return self.x, self.y
-end
-V2.__call = V2.unpack -- overload v()
-
-
---- Overload the tostring() operation
-function V2:__tostring()
-	return ("(%d,%d)"):format(self())
-end
-
--- tests (uncomment and check output)
---trace(2 * v2(11, 23)) --> (22,46)
-
--- end of "class V2"
--------------------------------------
-
 
 --
 -- TIC-80 utility functions
 --
 
--- Accepts {x=,y=} table or v2
 local function offset_screen(ox, oy)
-	poke(RAM.SCREEN_OFFSET+0, ox)
-	poke(RAM.SCREEN_OFFSET+1, oy)
+	poke(RAM.SCREEN_OFFSET+0, ox or 0)
+	poke(RAM.SCREEN_OFFSET+1, oy or 0)
 end
 
 
@@ -131,10 +73,8 @@ end
 -- GAME STATE                      --
 -------------------------------------
 
-offset = v2()
 
 function update()
-	offset.x = (t % 256) - 128
 end
 
 
@@ -142,7 +82,7 @@ function draw_overlay()
 	vbank(1)
 	cls()
 	
-	print(offset)
+	print(t)	
 	
 	vbank(0)
 end
@@ -152,6 +92,16 @@ end
 -------------------------------------
 
 t=0
+
+-- try to justify the "scene" part
+-- of the cartridge name
+function BDR(scanline)
+	local ox = 10 * math.sin(
+	            0.1 * (scanline + t))
+	local oy = 6 * math.cos(
+	            0.07 * (scanline + t))
+	offset_screen(ox, oy)
+end
 
 function TIC()
 	--------- update ----------
@@ -172,9 +122,7 @@ function TIC()
 	print("gwaaaaaaarrgh", 56, 20, 0)
 	-- dude
 	spr(83, 120, 40, 15)
-	
-	offset_screen(offset.x, offset.y)
-	
+		
 	draw_overlay()
 	
 	--------- advance t ----------
