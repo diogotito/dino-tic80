2a8aa0c46bad3d340ea98ae54fcf3dc639d6bd9b
diogotito <diogotitomarques@gmail.com>
2026-02-20 16:12:32 +0000

    Merge the cube into the scene

--- carts/playground/dinoscene.lua
+++ carts/playground/dinoscene.lua
@@ -137,12 +137,12 @@ function draw_overlay()
 	end
 	for i=1,m do
 		f(math.random(240),
-		     math.random(136),
-			 			math.random(240),
-		     math.random(136),
-					 	math.random(240),
-		     math.random(136),
-						 math.random(16))
+		  math.random(136),
+		  math.random(240),
+		  math.random(136),
+		  math.random(240),
+		  math.random(136),
+		  math.random(16))
 	end
 	
 ::NoMoreVFX::
@@ -195,12 +195,14 @@ function BDR(scanline)
 	offset_screen(ox, oy)
 end
 
+CUBE = {} -- filled by DINOCUBE code
+
 function TIC()
-	--------- update ----------
+	---------- update -----------
 	
 	update()
 	
-	---------- draw -----------
+	----------- draw ------------
 	
 	vbank(0)
 	
@@ -214,20 +216,229 @@ function TIC()
 	-- dino
 	spr(48, 20, 24, 0, 1, 0, 0, 3, 3)
 
-	if bar < 4 then return end		
+	if bar < 4 then return end
 	
 	line(42, 32, 55, 26, 0)
 	print(
 		("gwwaaaaaaaaarrgh"):sub(1, 1+2*music_row()),
 		56, 20, 0)
 	
+	----------- overlay ------------
 	
-	draw_overlay()
+	vbank(1) draw_overlay() vbank(0)
 	
-	--------- advance t ----------
+	---------- advance t -----------
 	
 	t = t + 1
+
+	-------- DINOCUBE TIC: ---------
+	
+	if bar >= 16 then
+		local oomph = music_row()%7/3-1
+		CUBE.z = 110 - 50*(1 - oomph^2)
+		vbank(1) CUBE.TIC() vbank(0)
+	end
+
 end
+
+
+-------------------------------------
+-- COPY OF DINOCUBE                --
+do
+	-- with readonly access to globals
+	setmetatable(CUBE, {__index=_ENV})
+	local _ENV = CUBE
+-- - - - - - - - - - - - - - - - - --
+
+-- DINOCUBE Geometry
+
+verts = {{-1, -1, -1},
+         { 1, -1, -1},
+         {-1,  1, -1},
+         { 1,  1, -1},
+         {-1, -1,  1},
+         { 1, -1,  1},
+         {-1,  1,  1},
+         { 1,  1,  1}}
+
+UVs = {{  0  , 3*8}, -- top    left
+       {3*8-4, 3*8}, -- top    right
+       {  0  , 6*8}, -- bottom left
+       {3*8-4, 6*8}} -- bottom right
+
+function mktri(i1,j1, i2,j2, i3,j3)
+	return {
+		verts={verts[i1],verts[i2],verts[i3]},
+		uvs={UVs[j1],UVs[j2],UVs[j3]}
+	}
+end
+
+cube_tris={
+	-- front face
+	mktri(1,1, 2,2, 3,3),
+	mktri(2,2, 4,4, 3,3),
+	-- right face
+	mktri(2,1, 6,2, 4,3),
+	mktri(4,3, 8,4, 6,2),
+	-- back face
+	mktri(5,2, 6,1, 7,4),
+	mktri(6,1, 8,3, 7,4),
+	-- left face
+	mktri(1,2, 5,1, 3,4),
+	mktri(3,4, 7,3, 5,1),
+	-- top face
+	mktri(5,1, 6,2, 1,3),
+	mktri(6,2, 2,4, 1,3),
+	--bottom face
+	mktri(7,2, 8,1, 3,4),
+	mktri(8,1, 4,3, 3,4),
+}
+
+
+-- DINOCUBE 3D maths!
+
+pi = math.pi
+sin, cos = math.sin, math.cos
+
+function transform(verts,
+                   rotX, rotY, rotZ,
+                   sX, sY, sZ,
+                   tX, tY, tZ)
+	-- rotation sines and cosines
+	local sinX,cosX = sin(rotX),cos(rotX)
+	local sinY,cosY = sin(rotY),cos(rotY)
+	local sinZ,cosZ = sin(rotZ),cos(rotZ)
+
+	for _, v in ipairs(verts) do
+		-- unpack vertex to nice variables
+		local vx,vy,vz = table.unpack(v)
+		-- rotate on X
+		vy,vz =  vy*cosX - vz*sinX,
+		         vy*sinX + vz*cosX
+		-- rotate on Y
+		vx,vz =  vx*cosY + vz*sinY,
+		        -vx*sinY + vz*cosY
+		-- rotate on Z
+		vx,vy =  vx*cosZ - vy*sinZ,
+		         vx*sinZ + vy*cosZ
+		-- scale
+		vx = sX * vx
+		vy = sY * vy
+		vz = sZ * vz
+		-- perspective!
+		vz = vz + tZ
+		local persp = vz / 100 -- idk rly
+		vx, vy = vx/persp, vy/persp
+		-- translate
+		vx = vx + tX
+		vy = vy + tY
+		-- update vertex coordinates
+		v[1],v[2],v[3] = vx,vy,vz
+	end
+end
+
+function render_tri(t)
+	ttri(t.verts[1][1], t.verts[1][2],
+	     t.verts[2][1], t.verts[2][2],
+	     t.verts[3][1], t.verts[3][2],
+	     t.uvs[1][1], t.uvs[1][2],
+	     t.uvs[2][1], t.uvs[2][2],
+	     t.uvs[3][1], t.uvs[3][2],
+	     0, -- read from SPRITES RAM
+	     {14}, -- chromakey
+	     t.verts[1][3], -- z1
+	     t.verts[2][3], -- z2
+	     t.verts[3][3]) -- z3
+end
+
+-- DINOCUBE
+-- Complement Lua's standard library
+
+function deepcopy(t) -- DINOCUBE 
+	local new={}
+	for k,v in pairs(t) do
+		if type(v)=="table" then
+			new[k]=deepcopy(v)
+		else
+			new[k]=v
+		end
+	end
+	return new
+end
+--[[
+function cat_tbls(...)
+	local tbls, cat = {...}, {}
+	for _, t in ipairs(tbls) do
+		table.move(t,1,#t, #cat+1,cat)
+	end
+	return cat
+end
+--]]
+function dump_tbl(t, i) -- DINOCUBE
+	i = i or 0
+	local out = {tostring(t)}
+	for k,v in pairs(t) do
+		out[#out+1] = ("[%s]=%s"):format(
+				k,
+				(type(v)=="table")
+					and dump_tbl(v, i+1)
+					or v)
+	end
+	local indent = string.rep(" ", i)
+	return indent ..
+			table.concat(out, "\n "..indent)
+end
+
+
+-- DINOCUBE Game loop
+
+t=0
+x=96
+y=60
+vx,vy=1,1
+z=100
+rX,rY,rZ = 0,0,0
+vrX,vrY,vrZ = 0.022, 0.055, 0.038
+
+function TIC() -- DINOCUBE
+	-- move it
+	x, y = x+vx, y+vy
+	if x<26  then vx= 1 vrY=-vrY end
+	if x>213 then vx=-1 vrY=-vrY end
+	if y<26  then vy= 1 vrX=-vrX end
+	if y>105 then vy=-1 vrX=-vrX end
+	
+	rX, rY, rZ = rX+vrX, rY+vrY, rZ+vrZ
+
+	-- draw it
+
+	local scn_tris  = {}
+	local scn_verts = {}
+
+	for _, t in ipairs(cube_tris) do
+		local tt = deepcopy(t)
+		table.insert(scn_tris, tt)
+		table.move(tt.verts, 1, 3,
+				#scn_verts + 1, scn_verts)
+	end
+
+	transform(scn_verts,
+	          rX, rY, rZ,
+	          20, 20, 20,
+	          x, y, z)
+
+	cls(0)
+	for _, t in ipairs(scn_tris) do
+		render_tri(t)
+	end
+
+	t=t+1
+end
+
+-- - - - - - - - - - - - - - - - - --
+end -- of DINOCUBE copypasta       --
+-------------------------------------
+
 -- <TILES>
 -- 000:9999999999999999999999999999999999999999999999999999999999999999
 -- 001:9999999999999999999999999999999999999999999999999999999999999999
@@ -279,5 +490,6 @@ end
 
 -- <PALETTE>
 -- 000:2f2a35443d386256539a5d40a981433b42626f7777989da0c2b8a9d9dbba000404ffffff000000000000000000000000
+-- 001:2f2a35443d386256539a5d40a981433b42626f7777989da0c2b8a9d9dbba000404ffffff000000000000000000000000
 -- </PALETTE>
 
