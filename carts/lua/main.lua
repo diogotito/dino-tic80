c882266e04cd3500cbb3200f48d9e170af0bc54f
diogotito <diogotitomarques@gmail.com>
2026-02-24 02:33:59 +0000

    Restore tq-bundler main.lua example

---
+++ carts/lua/main.lua
@@ -0,0 +1,15 @@
+-- title:   Dinolode
+-- author:  mimi, pati, tito, tomi
+-- desc:    motherlode but with dinosaurs
+-- site:    https://diogotito.com/experiments/dinoscene
+-- license: CC0-1.0
+-- version: 0.1
+-- script:  lua
+
+include "Vector2"
+
+function TIC()
+    cls(14)
+    local vec = v2(10) + v2(100, 0)
+    print("hi", vec.x, vec.y)
+end
