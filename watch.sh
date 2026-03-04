f0b1393cff97d568ca2496bbdb28c9a3a24a2c6a
diogotito <diogotitomarques@gmail.com>
2026-03-04 15:27:41 +0000

    Fix shell scripts for Linux

--- watch.sh
+++ watch.sh
@@ -5,4 +5,4 @@ dir=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd)
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${dir}/bin"
 
 cd "${dir}/carts" || exit
-exec "${dir}/bin/tic80" --fs="$(pwd)" --cmd="load dinolode.lua"
+exec "${dir}/bin/tq-bundler" run dinolode.lua lua/main.lua --tic "${dir}/bin/tic80"
